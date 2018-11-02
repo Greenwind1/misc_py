0b70ab3b9dbae235eb59bc95123574f1bceb4fb3
Maxwell <Greenwind120170@gmail.com>
2018-11-03 03:48:55 +0900

    artificial problem

---
+++ optimization/primal_dual_path_fm/artProb_init.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def artProb_init(M, q):
+    """
+
+    :array M: (n x n) matrix
+    """
+    assert isinstance(M, np.ndarray)
+    n.n = M.shape
