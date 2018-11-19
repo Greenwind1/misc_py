35bcae6ff59282318f168c2c42c9e6c2a8ea8409
Maxwell <Greenwind120170@gmail.com>
2018-11-19 09:05:12 +0900

    knapsack added

---
+++ optimization/bbm/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import gc
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# import warnings
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 100)
+pd.options.mode.chained_assignment = None
+# dir(pd.options.display)
+# warnings.simplefilter(action='ignore', category=FutureWarning)
+
+plt.style.use('ggplot')
---
+++ optimization/bbm/knapsack.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import gc
+import os
+import numpy as np
+
+
+class Knapsack(object):
+    def __init__(self, name):
+        self.name = name
+
+    def getbounds(self):
+        # compute the upper and lower bounds
+        exit()
+
+    def __str__(self):
+        return 'Name = ' + self.name
