e0a73c7102ae52e8f10050453f4112806d824b3a
Maxwell <Greenwind120170@gmail.com>
2018-11-23 03:47:33 +0900

    .

--- optimization/bbm/knapsack.py
+++ optimization/bbm/knapsack.py
@@ -6,8 +6,24 @@ import numpy as np
 
 
 class Knapsack(object):
-    def __init__(self, name):
+    def __init__(self, name, capacity, items, costs, weights,
+                 zeros=set(), ones=set()):
         self.name = name
+        self.capacity = capacity
+        self.items = items
+        self.costs = costs
+        self.weights = weights
+        self.zeros = zeros
+        self.ones = ones
+        self.lb = -100
+        self.ub = -100
+        ratio = {i: costs[i] / weights[i] for i in items}
+        self.sitemList = [
+            k for k, v in sorted(ratio, key=lambda x: x[1], reverse=True)
+        ]
+        self.xlb = {j: 0 for j in self.items}
+        self.xub = {j: 0 for j in self.items}
+        self.bi = None
 
     def getbounds(self):
         # compute the upper and lower bounds
